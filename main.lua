7c5611c4e414153d840a9c7a430058b9b4b839d4
ASUS <voronckov.danila@yandex.ru>
2020-04-01 16:05:23 +0300

    Импортирован код main и установлены исключения для файлов VS Code

--- main.lua
+++ main.lua
@@ -1,3 +1,11 @@
+-- основной алгоритм внешнего скрипта проекта
+IsMainExternal = true
 do
-    
+    lfs = require 'lfs' -- https://keplerproject.github.io/luafilesystem/manual.html
+    dofile(lfs.currentdir()..'/run/lib/WCLua/WCLua.lua')
+    --dofile('-','File')
+
+    --dofile('+TableParser','main')
+    --dofile('--','build')
 end
+print('\n\n\n\n\n'..tostring(0))
