7eb69e8df8d9bbda41c03302b105c701e7168128
ASUS <voronckov.danila@yandex.ru>
2020-04-04 17:22:10 +0300

    Добавлен логический параметр полного пути для метода split класса путей

--- run/lib/WCLuaStdScript/WCLua.lua
+++ run/lib/WCLuaStdScript/WCLua.lua
@@ -1,112 +1,4 @@
 do
-    tableInsert = table.insert
-    tableRemove = table.remove
-
-    function MergeTable(tbl1,tbl2)
-        for key, value in pairs(tbl2) do
-            tbl1[key] = value
-        end
-        return tbl1
-    end
-    function TableEmpty(tbl)
-        for _, _ in pairs(tbl) do
-            return false
-        end
-        return true
-    end
-
-    function isTable(var)
-        return type(var)=='table'
-    end
-    local _isTable = isTable
-    function isTable(var,tbl,...)
-        local result = {}
-        if not _isTable(tbl) then
-            local a = tbl
-            tbl = {}
-        end
-        if _isTable(tbl) then
-            for key, _ in pairs(tbl) do
-                result[key] = _isTable(tbl[key])
-            end
-        end
-        tbl = {...}
-        for i = 1, tbl do
-            result[i] = _isTable({...}[i])
-        end
-    end
-
-    function formatType(type)
-        local typeIsId = nil
-        if type=='number' or type=='string' or type=='boolean' or type=='table' or type=='function' then
-            return type
-        elseif type=='n' then
-            type='number'
-        elseif type=='s' or type=='id' then
-            type='string'
-            if type=='id' then
-                typeIsId = true
-            end
-        elseif type=='b' or type=='bn' then
-            type='boolean'
-        elseif type=='t' then
-            type='table'
-        elseif type=='f' then
-            type='function'
-        else
-            type=nil
-        end
-        return type,typeIsId
-    end
-    local inj = formatType
-    function formatType(type,tbl,...)
-        
-    end
-    
-    function isType(var,varType)
-        if not isTable(arr) then
-            return type(var)==formatType(varType)
-        end
-        tableInsert(arr,1,var)
-        tableInsert(arr,2,varType)
-        local c=0
-        for i = 1, #arr do
-            if i%2>0 then
-                var = arr[i]
-                c=c+1
-            else
-                local varType = formatType(arr[i])
-                if varType and type(var)==varType then
-                    c=c+1
-                end
-            end
-        end
-        return c==#arr
-    end
-    -- print(IsType(5,'number','abc','string',4,'string'))
-
-    function isTable(tbl,...)
-        return type(var) == 'table'
-    end
-    function isBoolean(var)
-        return type(var) == 'boolean'
-    end
-    function isNumber(var)
-        return type(var) == 'number'
-    end
-    function isString(var)
-        return type(var) == 'string'
-    end
-    function isFunction(var)
-        return type(var) == 'function'
-    end
-    function isClass(obj,class)
-        if not isTable(obj) then
-            return nil
-        end
-        return obj.class == tostring(class)
-    end
-
     local exts = {
         lua = '.lua',
         txt = '.txt',
@@ -125,8 +17,8 @@ do
     }
     Path = {}
     local function getPath(path)
-        if not isString(path) then
-            if isClass(path,Path) then
+        if type(path) ~= 'string' then
+            if path.class == tostring(Path) then
                 return path
             else
                 return nil
@@ -169,12 +61,17 @@ do
             return t.isDir
         end
         
-        function t.split()
+        function t.split(isFull)
             if t.dirPath or t.fileName or t.ext then
                 return t.dirPath, t.fileName, t.ext
             end
             local dirpath, filename, ext
-            local path = t.getFull()
+            local path
+            if isFull then
+                path = t.getFull()
+            else
+                path = t.path
+            end
             dirpath, filename = path:match("^%s*(.-)([^\\/]*)$")
             if filename then
                 filename, ext = filename:match("([^%.]*)%.?(.*)$")
@@ -201,13 +98,13 @@ do
             if t.filePath then
                 return t.filePath
             end
-            if isString(ext) then
+            if type(ext)=='string' then
                 local len = ext:len()
                 if ext:sub(1,1)=='.' and exts[ext:sub(2,len)] then
                     ext = ext:sub(2,len)
                 end
             end
-            local isExt = (isString(ext) and exts[ext])
+            local isExt = (type(ext)=='string' and exts[ext])
             t.fileExt = t.getFileExt()
             t.fullPath = t.getFull()
             if not t.fileExt then
@@ -245,12 +142,12 @@ do
         end
         return path.checkIsDir()
     end
-    function splitPath(path)
+    function splitPath(path,isFull)
         local path = getPath(path)
         if not path then
             return nil
         end
-        return path.split()
+        return path.split(isFull)
     end
     function getFileExt(path)
         local path = getPath(path)
@@ -266,30 +163,56 @@ do
         end
         return path.getFilePath(ext)
     end
-
-    local dofile_origin = dofile
-    function dofile(path,files)
-        if not (isString(path) and (isString(files) or isTable(files))) then
-            return nil
-        end
-        if isString(files) then
-            files = Path.getFilePath(files,'.lua')
-            local file = files
-            files = {}
-            tableInsert(files,file)
-        end
-        local dir = currentDir
-        if not Path.isDir(path) then
-            tableInsert(files,1,path)
-        else
-            currentDir = currentDir..path..'/'
-        end
-        for i = 1, #files do
-            local path = Path.getFilePath(files[i],'.lua')
-            if path then
-                dofile_origin(files[i])
+    do
+        return nil
+    end
+    if not LAUNCHED_IN_GAME then
+        local dofile_origin = dofile
+        function dofile(path,files)
+            local isDir = isDirPath(path)
+            if not (type(path)=='string' and (type(files)=='string' or type(files)=='table' or not isDir)) then
+                return nil
             end
+            -- local result = {}
+            -- for i = 1, #files do
+            --     local path = getFilePath(files[i],'.lua')
+            --     if path then
+            --         table.insert(result,path)
+            --     end
+            -- end
+            if not files then
+                files = {}
+            end
+            local dir = currentDir
+            if not isDir then
+                table.insert(files,1,path)
+
+                print(splitPath(path))
+            else
+                currentDir = currentDir..path..'/'
+            end
+            do
+                return nil
+            end
+            if type(files)=='string' then
+                files = getFilePath(files,'.lua')
+                local file = files
+                files = {}
+                table.insert(files,file)
+            end
+            -- for i = 1, #result do
+            --     print(currentDir)
+            --     dofile_origin(result[i])
+            -- end
+            for i = 1, #files do
+                local path = getFilePath(files[i],'.lua')
+                if path then
+                    dofile_origin(files[i])
+                end
+            end
+            -- print(currentDir)
+            currentDir = dir
         end
-        currentDir = dir
     end
 end
+dofile('_handles/_handles')
