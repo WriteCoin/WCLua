58863117694131701ed1913aa4950754bf8825a3
ASUS <voronckov.danila@yandex.ru>
2020-04-04 20:48:16 +0300

    _

--- main.lua
+++ main.lua
@@ -1,11 +1,10 @@
 -- основной алгоритм внешнего скрипта проекта
-IsMainExternal = true
+LAUNCHED_IN_GAME = (type(InitGlobals)=='function' and type(JASS_MAX_ARRAY_SIZE)=='number' and VERSION_REIGN_OF_CHAOS and VERSION_FROZEN_THRONE)
 do
     lfs = require 'lfs' -- https://keplerproject.github.io/luafilesystem/manual.html
-    dofile(lfs.currentdir()..'/run/lib/WCLuaStdScript/WCLua.lua')
-    --dofile('-','File')
-
-    --dofile('+TableParser','main')
-    --dofile('--','build')
+    projectDir = lfs.currentdir()..'\\'
+    currentDir = projectDir..'run/lib/WCLuaStdScript/'
+    dofile(currentDir..'WCLua.lua')
+    
 end
 print('\n\n\n\n\n'..tostring(0))
