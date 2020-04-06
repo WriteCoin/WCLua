91ad133b603020116da69d2e7d8e4f7706cc571c
ASUS <voronckov.danila@yandex.ru>
2020-04-06 16:39:59 +0300

    Подготовка к переходу на функциональное программирование

--- main.lua
+++ main.lua
@@ -1,10 +1,7 @@
 -- основной алгоритм внешнего скрипта проекта
-do
-    return nil
-end
 LAUNCHED_IN_GAME = (type(InitGlobals)=='function' and type(JASS_MAX_ARRAY_SIZE)=='number' and VERSION_REIGN_OF_CHAOS and VERSION_FROZEN_THRONE)
-local lfs = require('lfs') -- https://keplerproject.github.io/luafilesystem/manual.html
-projectDir = lfs.currentdir()..'\\'
-currentDir = projectDir..'run/lib/WCLuaStdScript/'
-dofile(currentDir..'WCLua.lua')
-print('\n\n\n\n\n'..tostring(0))
+local path = 'run/lib/WCLuaStdScript/WCLua.lua'
+dofile(path)
+if not LAUNCHED_IN_GAME then
+    print('\n\n\n\n\n'..tostring(0))
+end
